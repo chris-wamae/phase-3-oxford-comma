7ff1862c6a1dd62a0e12f5b041bbfccdf65fe2af
chris-wamae <chriswamae123@gmail.com>
2023-02-08 05:13:34 +0300

    Completed Assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+    # join the arrays with commas for all the elements
+    # except between the second last and last for which 
+    # ---remove the last element
+    # ---use join on all the others
+    # ---join and to the removed element
+    # ---join the two strings
+    # and should be used
+ 
+case 
+when array.length == 1 then array.join
+when array.length == 2 then array.join(" and ")
+when array.length > 2
+array.slice(0,array.length-1).join(", ") + ", and " + array.slice(array.length-1,array.length-1).join
 end
+end
+
+
